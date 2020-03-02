86e6c2645b92ee169b894a71a4527a6f655e25a8
Josiah <reikiryo@yahoo.com>
2020-03-02 17:54:04 -0500

    adds routes for each component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
+import { PrivateRoute } from './utils/PrivateRoute'
+
+import LandingPage from './components/LandingPage'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import PostOrSave from './components/PostOrSave'
+import SavedPosts from './components/SavedPosts'
+import PostForm from './components/PostForm'
+import Post from './components/Post';
 
 function App() {
    return (
       <div className="App">
-         App
+         <Route exact path='/' component={LandingPage} />
+         <Route path='/login' component={Login} />
+         <Route path='/sign-up' component={SignUp} />
+
+         <PrivateRoute exact path='/home' component={PostOrSave} />
+         <PrivateRoute exact path='/posts' component={SavedPosts} />
+         <PrivateRoute exact path='/analyze' component={PostForm} />
+         <PrivateRoute exact path='/posts/:id' component={Post} />
       </div>
    );
 }
--- src/components/PostOrSave.js
+++ src/components/PostOrSave.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const PostOrSave = () => {
+
    return (
       <div>
          Post / Save
