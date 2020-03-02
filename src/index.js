f351d8541fddfc1cc24d4d70f38ee75e2dbfaec3
Josiah <reikiryo@yahoo.com>
2020-03-02 13:13:02 -0500

    fixes syntax errors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'redux'
-import { createStore, applyMiddleware } from 'react-redux'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import thunk from 'redux-thunk'
 
 import './index.css';
 import App from './App';
-import reducer from './reducers'
+import { reducer } from './reducers'
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
