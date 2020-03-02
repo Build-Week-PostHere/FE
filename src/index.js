42489380c580b6c428b7efa44ebab0523d019360
Josiah <reikiryo@yahoo.com>
2020-03-02 10:13:50 -0500

    redux boilerplate index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'redux'
+import { createStore, applyMiddleware } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import thunk from 'redux-thunk'
+
 import './index.css';
 import App from './App';
+import reducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+   <Router>
+      <Provider store={store}>
+         <App />
+      </Provider>
+   </Router>
+   , document.getElementById('root'));
