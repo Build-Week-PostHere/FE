abbd818074e86247d246babc92c1e18fb1afc4b6
Josiah <reikiryo@yahoo.com>
2020-03-02 10:27:44 -0500

    redux boilerplate, reducers

---
+++ src/reducers/editReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+   isEditing: false,
+   postToEdit: {}
+}
+
+export const editReducer = (state = initialState, action) => {
+   switch (action.type) {
+      default:
+         return state
+   }
+}
---
+++ src/reducers/loginReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+   isFetching: false,
+   error: ''
+}
+
+export const loginReducer = (state = initialState, action) => {
+   switch (action.type) {
+      default:
+         return state
+   }
+}
