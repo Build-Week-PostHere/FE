81cbacceaee3e84b78be26bd06a1ea83672f2c15
Josiah <reikiryo@yahoo.com>
2020-03-02 15:48:57 -0500

    misc

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -1,7 +1,17 @@
+import {
+   REGISTER,
+   REGISTER_SUCCESS,
+   REGISTER_FAIL,
+   LOGIN,
+   LOGIN_SUCCESS,
+   LOGIN_FAIL
+} from '../actions/loginActions'
+
 const initialState = {
    isFetching: false,
    token: '',
-   error: ''
+   error: '',
+   registerError: ''
 }
 
 export const loginReducer = (state = initialState, action) => {
