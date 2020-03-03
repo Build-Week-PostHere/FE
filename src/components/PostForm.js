179d3cd18c9918d707d55d1f11d6876d3cb2a4ee
Josiah <reikiryo@yahoo.com>
2020-03-03 17:06:07 -0500

    editing actions functional

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,21 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+
+import { addPost, editPost } from '../actions/crudActions'
 
 const PostForm = () => {
-   const [post, setPost] = useState({title:'', body:''});
+   const [post, setPost] = useState({ title: '', body: '' });
 
    const handleChange = e => {
-      setPost({...post, [e.target.name]: e.target.value});
+      setPost({ ...post, [e.target.name]: e.target.value });
+   }
+
+   const handleSubmit = e => {
+      e.preventDefault()
+
    }
 
    return (
       <div>
          <form>
-            <input type='text' name='title' placeholder='Write your title here.' value={post.title} onChange={handleChange}/><br/>
-            <textarea name='body' placeholder='Write your post here.' value={post.body} onChange={handleChange} /><br/>
+            <input type='text' name='title' placeholder='Write your title here.' value={post.title} onChange={handleChange} /><br />
+            <textarea name='body' placeholder='Write your post here.' value={post.body} onChange={handleChange} /><br />
             <input type='submit' value='Submit Post' />
          </form>
       </div>
    );
 }
 
-export default PostForm;
+export default connect()(PostForm);
