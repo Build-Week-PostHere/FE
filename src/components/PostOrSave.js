486c07d16b1259322a6ff25ce2ecade6daaeae16
Sara Reidy <sara.reidy.21@gmail.com>
2020-03-03 21:18:27 -0500

    Added button styles on home

--- src/components/PostOrSave.js
+++ src/components/PostOrSave.js
@@ -6,7 +6,7 @@ import { logout } from '../actions/loginActions'
 
 //Asset Imports
 import AlienLogo from '../assets/AlienLogo';
-import Logout from '../assets/Logout';
+import exit from '../assets/exit.svg'
 import analytics from '../assets/analytics.svg';
 import save from '../assets/save.svg';
 
@@ -23,16 +23,29 @@ const PostOrSave = ({ logout }) => {
                <h2>Analyze a Post</h2>
                <img src={analytics} alt='Analytics' />
                <p>Tell us what you want to post and we'll tell you where to post it.</p>
+               <Link to='/analyze'>
+                  <button class='inner-button'> 
+                     Analyze
+                  </button>
+               </Link>
             </div>
             <div className='saved-or-post-sub-container'>
                <h2>View Saved Posts</h2>
                <img src={save} alt='Saved' />
                <p>Look back on posts you've already had analyzed.</p>
+               <Link to='/posts'>
+                  <button class='inner-button'> 
+                     Saved Posts
+                  </button>
+               </Link>
             </div>
          </div>
-         <Link to='/analyze'><button >Analyze</button></Link>
-         <Link to='/posts'><button >Saved</button></Link>
-         <Logout onClick={logout}>Logout</Logout>
+         <Link to='/'>
+            <button onClick={logout} class='logout-button'> 
+               Log Out 
+               <img src={exit} alt=''/>
+            </button>
+         </Link>
       </div>
    );
 }
