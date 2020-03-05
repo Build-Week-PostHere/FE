806a97c30bf51ae41828b7cb64f08c674ac270af
Josiah <reikiryo@yahoo.com>
2020-03-05 18:27:06 -0500

    post styles

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,7 +8,7 @@ import { deletePost, getSpecific } from '../actions/crudActions'
 import Header from './Header'
 
 const Post = ({ post, getSpecific, toggleEdit, setPostToEdit, deletePost }) => {
-   const { post_title, post_text, post_sub_reddit } = post
+   const { post_title, post_text, post_sub_reddit, dated } = post
    const id = useParams().id
    const history = useHistory()
 
@@ -28,15 +28,20 @@ const Post = ({ post, getSpecific, toggleEdit, setPostToEdit, deletePost }) => {
    }
 
    return (
-      <div>
+      <div className='post-div'>
          <div className='home-header'>
             <Header />
          </div>
          <h1 className='post-h1'>Suggested Subreddit: <Link to={`/subreddit/${post_sub_reddit}`}>{post_sub_reddit}</Link></h1>
-         <button className='post-btn' onClick={handleEdit} class='med-button'>Update Post</button>
-         <button className='post-btn' onClick={handleDelete} class='small-button'>X</button>
-         <h3 className='post-h3' >{post_title}</h3>
-         <h4 className='post-h4' >{post_text}</h4>
+         <div className='post-top-div'>
+            <h3>{dated}</h3>
+            <div className='post-btn-div'>
+               <button className='post-btn' onClick={handleEdit} >Update Post</button>
+               <button className='post-btn' onClick={handleDelete} >X</button>
+            </div>
+         </div>
+         <div className='post-div1'><h3 >{post_title}</h3></div>
+         <div className='post-div2'><h4 >{post_text}</h4></div>
       </div>
    );
 }
